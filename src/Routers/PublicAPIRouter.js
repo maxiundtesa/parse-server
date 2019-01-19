249e9d9244dad91a51b4e280c33f3ea647bf77ad
Maximilian Fuchs <maxifuchs98@gmail.com>
2019-01-19 16:01:26 +0100

    Added MailAusgabe

--- src/Routers/PublicAPIRouter.js
+++ src/Routers/PublicAPIRouter.js
@@ -74,9 +74,10 @@ export class PublicAPIRouter extends PromiseRouter {
 
     return userController.resendVerificationEmail(username).then(
       () => {
+        const params = qs.stringify({ mail });
         return Promise.resolve({
           status: 302,
-          location: `${config.linkSendSuccessURL}`,
+          location: `${config.linkSendSuccessURL}?${params}`,
         });
       },
       () => {
